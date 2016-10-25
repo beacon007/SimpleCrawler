fa3f30490d67da8805db9480de98c2a4faad684c
BiKun <zenith_94@outlook.com>
2016-10-25 15:52:09 +0800

    img_crawler增加多线程

--- img_crawler.py
+++ img_crawler.py
@@ -1,11 +1,30 @@
-import urllib,os
+import urllib,os,time
 import lxml.html.soupparser as sp
+import threading
 from link_crawler import LinkCrawler
 from downloader import Downloader
 
-def zhihu_img_crawler(url_from_zhihu):
-    for link in LinkCrawler(url_from_zhihu,"/question/\d+$").link_crawler():
-        get_images_from_zhihu(Downloader().download(link))
+def zhihu_img_crawler(url_from_zhihu,max_threads=10):
+    threads = []
+    links = LinkCrawler(url_from_zhihu,"/question/\d+$").link_crawler()
+    while threads or links:
+        # the crawl is still active
+        for thread in threads:
+            if not thread.is_alive():
+                # remove the stopped threads
+                threads.remove(thread)
+        while len(threads) < max_threads and links:
+            # can start some more threads
+            thread = threading.Thread(target=get_images_from_zhihu(Downloader().download(links.pop())))
+            # set daemon so main thread can exit when receives ctrl-c
+            thread.setDaemon(True)
+            thread.start()
+            threads.append(thread)
+
+        # all threads have been processed
+        # sleep temporarily so CPU can focus execution elsewhere
+        time.sleep(SLEEP_TIME)
+
 
 def get_images_from_zhihu(html):
     path = os.path.abspath(IMAGE_PATH)
@@ -29,7 +48,7 @@ def store_image(url, path, filename):
 #         if key_word in dom.xpath('//*[@class="zm-tag-editor-labels zg-clear"]//a/text()'):
 #             return True
 
-
+SLEEP_TIME=1
 IMAGE_PATH="zhihu_images/"
 
 if __name__=="__main__":
