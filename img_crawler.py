4bf61d9648d231ce0988b6a7e41229cedcb46089
BiKun <zenith_94@outlook.com>
2016-11-22 23:27:48 +0800

    update

--- img_crawler.py
+++ img_crawler.py
@@ -1,7 +1,7 @@
 #coding: utf-8
 import urllib,os
 import lxml.html.soupparser as sp
-import thread,threading
+import threading
 from link_crawler import LinkCrawler
 from downloader import Downloader
 
@@ -10,21 +10,34 @@ def zhihu_img_crawler(url_from_zhihu):
     if not os.path.exists(IMAGE_PATH):
         os.mkdir(IMAGE_PATH)
     for link in LinkCrawler(url_from_zhihu,"/question/\d+$").link_crawler():
-        get_images_from_zhihu(Downloader().download(link))
+        get_images_from_zhihu(Downloader().download(link),10)
 
 
 
-def get_images_from_zhihu(html):
+def get_images_from_zhihu(html,max_threads):
     dom = sp.fromstring(html)
-    for img_link in dom.xpath('//*[@id="zh-question-answer-wrap"]//img[@data-original]/@data-original'):
-        print("Get img_link :  " + img_link)
-        if (threading.enumerate().__len__()<5):
-            thread.start_new_thread(store_image,(img_link,img_link[23:]))
+    """单线程"""
+    # for img_link in dom.xpath('//*[@id="zh-question-answer-wrap"]//img[@data-original]/@data-original'):
+    #     print("Get img_link :  " + img_link)
+    #     store_image(img_link)
+    """多线程"""
+    links = dom.xpath('//*[@id="zh-question-answer-wrap"]//img[@data-original]/@data-original')
+    threads = []
+    while threads or links:
+        for thread in threads:
+            if not thread.is_alive():
+                threads.remove(thread)
+        while len(threads) < max_threads and links:
+            link = links.pop()
+            print("Get img_link :  " + link)
+            thread = threading.Thread(store_image(link))
+            thread.setDaemon(True)
+            thread.start()
+            threads.append(thread)
 
-
-def store_image(url, filename):
+def store_image(url):
     path = os.path.abspath(IMAGE_PATH)
-    dest_dir = os.path.join(path, filename)
+    dest_dir = os.path.join(path, url[23:])
     if not os.path.exists(dest_dir):
         print ("Downloading img...")
         urllib.urlretrieve(url, dest_dir)
@@ -34,13 +47,8 @@ def store_image(url, filename):
 IMAGE_PATH="zhihu_images/"
 
 if __name__=="__main__":
-    # links=LinkCrawler("https://www.zhihu.com/topic/19552207/hot", "/question/\d+$").link_crawler()
-    # ThreadCrawler(Downloader().download(links[0])).start()
-    # ThreadCrawler(Downloader().download(links[1])).start()
-    # ThreadCrawler(Downloader().download(links[2])).start()
-    # ThreadCrawler(Downloader().download(links[3])).start()
-    # ThreadCrawler(Downloader().download(links[4])).start()
-    zhihu_img_crawler("https://www.zhihu.com/topic/19552207/hot")
+
+    zhihu_img_crawler("https://www.zhihu.com/topic/19552207/top-answers")
 
 
 
